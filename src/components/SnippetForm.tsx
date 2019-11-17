f1fdfed3e1e15d895b8574d00cdb8e75423c14a0
Kazuya Izumi <shikibu9419@gmail.com>
2019-11-18 00:58:18 +0900

    Add button to export input data as TOML string

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
 
+const tomlStream = require('toml-stream');
+
 interface FormState {
-    text: string
+  name?: string,
+  body?: string,
+  prefix?: string,
+  description?: string,
 }
 
 export default class SnippetForm extends React.Component<{}, FormState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            text: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: '',
+      body: '',
+      prefix: '',
+      description: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.export = this.export.bind(this);
+  }
 
-    handleChange(e: any) {
-        this.setState({text: e.target.value});
-    }
+  handleChange(e: any) {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
-    render() {
-        return (
-            <div className="form-wrapper">
-                <textarea style={{ height: '100%', width: '100%' }} value={this.state.text} onChange={this.handleChange} />
-            </div>
-        )
-    }
+  export(e: any) {
+    console.log('exported toml:');
+    tomlStream.toTOMLString(this.state, (er: any, output: string) => {
+      if (er) throw er
+      console.log(output)
+    })
+  }
+
+  render() {
+    return (
+      <div className="form-wrapper">
+        <input type="text" style={{ height: '100%', width: '100%' }} name="name" value={this.state.name} onChange={this.handleChange} />
+        <input type="text" style={{ height: '100%', width: '100%' }} name="prefix" value={this.state.prefix} onChange={this.handleChange} />
+        <textarea style={{ height: '100%', width: '100%' }} name="description" value={this.state.description} onChange={this.handleChange} />
+        <textarea style={{ height: '100%', width: '100%' }} name="body" value={this.state.body} onChange={this.handleChange} />
+        <button onClick={this.export}>export</button>
+      </div>
+    )
+  }
 }
